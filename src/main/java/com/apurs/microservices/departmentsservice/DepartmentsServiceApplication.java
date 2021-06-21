b4b9501d5e25f2ee6a5ed518a662f0eab4bd8f2d
Stefan Katic <katicstefan.coder@gmail.com>
2021-06-21 18:24:50 +0200

    first commit

---
+++ src/main/java/com/apurs/microservices/departmentsservice/DepartmentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.departmentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apurs/microservices/departmentsservice/DepartmentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.departmentsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
